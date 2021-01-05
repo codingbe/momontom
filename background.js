be76f89203e6b41d315c455ef5c168662705df7f
codingbe <descuento@naver.com>
2021-01-05 19:03:23 +0900

    CSS수정 및 삭제아이콘 변경

---
+++ background.js
@@ -0,0 +1,55 @@
+const image = [
+  {
+    id: 1,
+    link:
+      "https://images.pexels.com/photos/853199/pexels-photo-853199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 2,
+    link:
+      "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 3,
+    link:
+      "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 4,
+    link:
+      "https://images.pexels.com/photos/1229042/pexels-photo-1229042.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 5,
+    link:
+      "https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 6,
+    link:
+      "https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 7,
+    link:
+      "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 8,
+    link:
+      "https://images.pexels.com/photos/1213447/pexels-photo-1213447.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+];
+
+const body = document.querySelector("body");
+
+const randomBackground = () => {
+  const random = Math.floor(Math.random() * parseInt(image.length));
+  body.style.background = `url(${image[random].link}) no-repeat center center fixed`;
+};
+
+function init() {
+  randomBackground();
+}
+
+init();
